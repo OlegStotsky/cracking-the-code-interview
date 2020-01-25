5beaf10d9b09b6ab82d4ddd00eb0e6e283c6b002
Oleg Stotsky <ostotsky@gmail.com>
2020-01-25 14:40:41 +0300

    add Chapter2.Task1

---
+++ src/com/olegstotsky/chapter2/Task1.java
@@ -0,0 +1,80 @@
+package com.olegstotsky.chapter2;
+
+import java.util.HashSet;
+
+class Node {
+    public Node next = null;
+    public int num;
+
+    public Node(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Node{" +
+                "num=" + num +
+                '}';
+        if (next != null) {
+            s += next.toString();
+        }
+        return s;
+    }
+}
+
+public class Task1 {
+    //O(N) memory, O(N) time
+    public static Node removeDuplicates(Node node) {
+        Node head = node;
+        HashSet<Integer> s = new HashSet<>();
+        Node prev = null;
+        while (node != null) {
+            Node next = node.next;
+            if (s.contains(node.num)) {
+                prev.next = next;
+            } else {
+                prev = node;
+                s.add(node.num);
+            }
+            node = next;
+        }
+
+        return head;
+    }
+
+    // O(1) memory, O(N^2) time
+    public static Node removeDuplicatesConstMemory(Node node) {
+        Node head = node;
+        while (node != null) {
+            Node runner = node.next;
+            while (runner != null && runner.num == node.num) {
+                runner = runner.next;
+            }
+            node.next = runner;
+            node = runner;
+        }
+
+        return head;
+    }
+
+    public static Node genList(int... arr) {
+        Node ans = null;
+        Node head = null;
+        for (int x : arr) {
+            Node curNode = new Node(x);
+            if (ans == null) {
+                ans = curNode;
+                head = curNode;
+            } else {
+                ans.next = curNode;
+                ans = curNode;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(genList(1, 1, 2, 3, 3, 4, 4, 4, 1, 1, 1, 1, 1, 5)).toString());
+    }
+}
