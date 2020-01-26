95b843e128d130fc69dd7b43e843ecfd71137b80
Oleg Stotsky <ostotsky@gmail.com>
2020-01-27 02:25:52 +0300

    add chapter.Task2 O(1) memory solution

--- src/com/olegstotsky/chapter2/Task2Correct.java
+++ src/com/olegstotsky/chapter2/Task2Correct.java
@@ -1,6 +1,7 @@
 package com.olegstotsky.chapter2;
 
 public class Task2Correct {
+    //O(N) memory, O(N) time
     public static int kthToLast(Node head, int k) {
         if (head == null) {
             return 0;
@@ -12,8 +13,24 @@ public class Task2Correct {
         return pos;
     }
 
+    // O(1) memory, O(N) time
+    public static Node kthToLastV2(Node head, int k) {
+        Node slow = head;
+        Node fast = head;
+        for (int i = 0; i < k; ++i) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
     public static void main(String[] args) {
         Node l = Utils.genList(5, 4, 200, 100);
-        kthToLast(l, 2);
+        System.out.println(kthToLastV2(l, 2).toString());
     }
 }
