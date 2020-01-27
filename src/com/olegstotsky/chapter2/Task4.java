01942ddc72faa47cb6272ae6723e5700092a6647
Oleg Stotsky <ostotsky@gmail.com>
2020-01-27 19:02:37 +0300

    add Chapter2.Task4

---
+++ src/com/olegstotsky/chapter2/Task4.java
@@ -0,0 +1,42 @@
+package com.olegstotsky.chapter2;
+
+public class Task4 {
+    //5 4 3 2 1
+    //3 2 1 4 5
+    public static Node partition(Node head, int val) {
+        Node left = null;
+        Node right = null;
+        Node leftHead = null;
+
+        while (head != null) {
+            Node next = head.next;
+            if (head.num < val) {
+                if (left == null) {
+                    left = head;
+                    leftHead = head;
+                } else {
+                    left.next = head;
+                    left = head;
+                }
+            } else {
+                if (right == null) {
+                    right = head;
+                    right.next = null;
+                } else {
+                    head.next = right;
+                    right = head;
+                }
+            }
+
+            head = next;
+        }
+
+        left.next = right;
+        return leftHead;
+    }
+
+    public static void main(String[] args) {
+        Node n = Utils.genList(5, 4, 4, 3, 3, 2, 1);
+        System.out.println(partition(n, 3).toString());
+    }
+}
