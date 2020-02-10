2132022b673db771d89a33c2fd25927429aba4e7
Oleg Stotsky <ostotsky@gmail.com>
2020-02-10 12:41:19 +0300

    chapter 3: update Queue

--- src/com/olegstotsky/chapter3/Queue.java
+++ src/com/olegstotsky/chapter3/Queue.java
@@ -1,4 +1,26 @@
 package com.olegstotsky.chapter3;
 
-public class Queue {
+public class Queue<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    void enqueue(T val) {
+        if (head == null) {
+            head = new Node<T>(val);
+            tail = head;
+            return;
+        }
+        Node<T> node = new Node<>(val);
+        tail.next = node;
+        tail = node;
+    }
+
+    T dequeue() {
+        if (head == null) {
+            return null;
+        }
+        Node<T> tmp = head;
+        head = head.next;
+        return tmp.val;
+    }
 }
